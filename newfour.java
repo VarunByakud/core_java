ed22db6a3aaca155a280413caf8527161514d0a1
VarunByakud <varunbyakud14@gmail.com>
2025-06-18 10:13:16 +0530

    sop

---
+++ newfour.java
@@ -0,0 +1,5 @@
+class newfour{
+	public static void main(String args[]){
+		System.out.println("come to dhubai");
+	}
+}
--- newseven.java
+++ newseven.java
@@ -1,4 +1,4 @@
-class Hello{
+class newseven{
 	public static void main(String args[]){
 		System.out.println("Hello, World!");
 	}
